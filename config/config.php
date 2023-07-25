d38df374ed7c3e2bde28b1b2dbe9b1f456a798fa
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-07-25 10:02:59 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 
 return [
     'enable' => true, //Enable or disable support
-    'support_users' => [], //An array of support user ids. If you have live-controls/groups installed, use support_groups instead!
+    'support_users' => [1], //An array of support user ids. If you have live-controls/groups installed, use support_groups instead!
     'support_groups' => ['admin'] //An array of support group keys. You need to have live-controls/groups package installed to use it!
 ];
