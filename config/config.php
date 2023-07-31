2e1749b5a9e183405ada2944716d1e7f9fa5c8a8
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-07-31 12:16:45 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,5 +3,7 @@
 return [
     'enable' => true, //Enable or disable support
     'support_users' => [1], //An array of support user ids. If you have live-controls/groups installed, use support_groups instead!
-    'support_groups' => ['admin'] //An array of support group keys. You need to have live-controls/groups package installed to use it!
+    'support_groups' => ['admin'], //An array of support group keys. You need to have live-controls/groups package installed to use it!
+    'layout' => 'x-app-layout', //Choose the layout (without tag characters) from your list of layouts
+    'route_prefix' => 'support' //The prefix used for the routes
 ];
