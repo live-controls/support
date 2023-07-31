6b421393851cfa397240498c508ad9a6c2fb1ee7
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-07-31 10:28:21 -0300

    Added FormRequest Validations

---
+++ src/Http/Requests/UpdateSupportMessageRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LiveControls\Support\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSupportMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'priority' => 'required|integer|max:255',
+            'content' => 'required|string',
+            'support_ticket_id' => 'required'
+        ];
+    }
+}
